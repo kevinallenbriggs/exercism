a90a11349693213e91c48ee7123156ab1a804497
Kevin Briggs <kevinb@psaudio.com>
2020-05-11 07:41:43 -0600

    submit first iteration of WordCount in PHP track

--- php/word-count/word-count.php
+++ php/word-count/word-count.php
@@ -1,1 +1,20 @@
 <?php
+
+/**
+ * Given a phrase, count the occurrences of each word in that phrase.
+ *
+ * @param string $phrase
+ * @return array
+ */
+function wordCount(string $phrase): array
+{
+    // split the phrase into individual words
+    $words = preg_split(
+        '/[\s]+/',      // split by whitespace
+        preg_replace('/[^a-z0-9\s?]/', '', strtolower($phrase)), // sanitize
+        NULL,   // no limit
+        PREG_SPLIT_NO_EMPTY     // don't return empty words
+    );
+
+    return array_count_values($words);  // count 'em up
+}
